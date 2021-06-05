a23782691dcbfc7c144ff55551fb2f666266303e
William Faircloth <>
2021-06-05 23:47:14 +0000

    create express/bs app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-// Edit me
+// Create Express app
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Setup EJS
+const ejs = require('ejs');
+app.engine('ejs', ejs.renderFile);
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+// Main routing
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Listen on port 8080
+app.listen(8080);
