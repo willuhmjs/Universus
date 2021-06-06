cd0034b2e7cca23f1c904cfcf3029072e06dc997
William Faircloth <>
2021-06-06 21:15:25 +0000

    fix trivial, add random placeholder js

--- src/public/placeholder.js
+++ src/public/placeholder.js
@@ -1,6 +1,3 @@
-try {
 let input = document.getElementById("link_input");
-let choice = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", ]
-} catch(e) {
-  alert(e)
-}
+let choice = ['https://www.youtube.com/watch?v=dQw4w9WgXcQ', 'https://www.instagram.com/p/BsOGulcndj-/?utm_medium=copy_link', 'https://twitter.com/elonmusk/status/1399985389725753346/photo/1', 'https://open.spotify.com/track/2GhVG8MEumQLI7ko9elqpr'][Math.floor(Math.random() * 4)];
+input.placeholder = choice;
