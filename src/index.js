f187b9bee67d9e4b56fefd64c18405e6e3c4a074
William Faircloth <>
2021-06-07 00:49:37 +0000

    Add automated copyright + trivial

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 // Get platformFinder, downloader
 const { Youtube } = require('./downloader');
-const platformFinder = require('./platformFinder')
+const platformFinder = require('./platformFinder');
 
 // Setup body-parser
 const bodyParser = require('body-parser');
@@ -20,43 +20,52 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Main routing
 app.get('/', (req, res) => {
-  res.render('index', {textElement: '', brTag: ''});
+	res.render('index', { textElement: '', brTag: '' });
 });
 
 app.post('/preview', async (req, res) => {
-  try {
-    let platform = platformFinder(req.body.link);
-    if (!platform) return res.render('index.ejs', {textElement: '<p style="color: red; margin-top: 0.5rem;">Provided link must be a valid platform!</p>', brTag: '<br>'})
-  let exportData = {
-    thumbnailBool: false,
-    thumbnail: "",
-    platform: "",
-    title: "",
-    link: "",
-    id: ""
-  };
-  switch(platform) {
-    case 'youtube':
-      const youtube = new Youtube(req.body.link);
-      res.json(await youtube.getMeta())
-      //res.send('youtube');
-      break;
-    case 'spotify':
-      res.send('spotify');
-      break;
-    case 'facebook':
-      res.send('facebook');
-      break;
-    case 'instagram':
-      res.send('instagram');
-      break;
-    case 'twitter':
-      res.send('twitter');
-  }
-  } catch(e) {
-    return res.render('index.ejs', {textElement: '<p style="color: red; margin-top: 0.5rem;">Provided link must be a valid platform1</p>', brTag: 'br>'})
-  }
-})
+	try {
+		let platform = platformFinder(req.body.link);
+		if (!platform)
+			return res.render('index.ejs', {
+				textElement:
+					'<p style="color: red; margin-top: 0.5rem;">Provided link must be a valid platform!</p>',
+				brTag: '<br>'
+			});
+		let exportData = {
+			thumbnailBool: false,
+			thumbnail: '',
+			platform: '',
+			title: '',
+			link: '',
+			id: ''
+		};
+		switch (platform) {
+			case 'youtube':
+				const youtube = new Youtube(req.body.link);
+				res.json(await youtube.getMeta());
+				//res.send('youtube');
+				break;
+			case 'spotify':
+				res.send('spotify');
+				break;
+			case 'facebook':
+				res.send('facebook');
+				break;
+			case 'instagram':
+				res.send('instagram');
+				break;
+			case 'twitter':
+				res.send('twitter');
+		}
+	} catch (e) {
+		return res.render('index.ejs', {
+			textElement:
+				'<p style="color: red; margin-top: 0.5rem;">Provided link must be a valid platform1</p>',
+			brTag: 'br>'
+		});
+	}
+});
 
 // Listen on port 8080
 app.listen(8080);
